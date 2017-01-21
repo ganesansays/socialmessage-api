3fb839372e37d81f78e855bcf5c42c80ac2c3b57
ganesan <ganesansays@gmail.com>
2017-01-21 18:24:45 +0530

    added api-docs end point

--- app.js
+++ app.js
@@ -14,22 +14,21 @@ var config = {
 // Connect to the socialPosts MongoDB
 mongoose.connect(process.env.MONGODB_URI);
 
-// if(!process.env.CI) {
-  var admin = require('firebase-admin');
-  var serviceAccount =  JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+var admin = require('firebase-admin');
+var serviceAccount =  JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
 
-  //console.log(JSON.stringify(serviceAccount));
+app.locals.defaultFirebaseApp = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+});
 
-  app.locals.defaultFirebaseApp = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-  });
-// }
-
+app.get('/api-docs', function(req, res){
+    res.sendFile('swagger.yaml', {root: './api/swagger'});
+});
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
@@ -40,7 +39,8 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   var port = process.env.PORT || 10010;
   app.listen(port);
 
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.info('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
+  // if (swaggerExpress.runner.swagger.paths['/hello']) {
+  //   console.info('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+  // }
+  console.info('Server started on port: ' + port);
 });
